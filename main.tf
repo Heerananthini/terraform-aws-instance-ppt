efaa001d09a97abbaba755d5f5a0ed287b5c40ec
Heera <71284609+Heerananthini@users.noreply.github.com>
2022-09-13 13:06:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "vpc-ppt" {
 resource "aws_instance" "web" {
   ami               = var.ami
   instance_type     = var.instance_type
-   subnet_id = aws_subnet.web_subnet.id
+   subnet_id = vpc-ppt.subnet_id
   tags              = {
      "Name" = "web"
   }
